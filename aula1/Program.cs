114cd2aeef19bc6c931b01888b0f750823e82708
FIC <FIC@info.local>
2019-09-13 11:09:22 -0300

    exercício das médias

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -11,13 +11,36 @@ namespace aula1
             //declarada e com valor atribuído
             int num2 = 5;
             //declaradas diversas variáveis de mesmo tipo, com ou sem valor atribuido
-            int num3, num4, num5 = 7;
+            int num3, num4;
             //variavel do tipo decimal(real)
             float media;
             //variavel de retorno verdadeiro ou falso - tipo lógico
             bool resultado = false;
+            //variavel do tipo texto
+            string nomeAluno;
+            //escreve em uma linha e quebra-a
+            Console.WriteLine("Bem vindos à nossa primeira aplicação!");
+            //continua escrevendo na mesma linha
+            Console.Write("Digite seu nome, corno: ");
+            //pega o dado digitado pelo usuário e salva na variável
+            nomeAluno = Console.ReadLine();
+            //concatenação
+            Console.WriteLine(nomeAluno + ", agora digite sua primeira nota:");
+            //converte para inteiro antes de armazenar o valor na variável
+            num1 = int.Parse(Console.ReadLine());
+            //interpolação
+            Console.WriteLine($"Sua primeira nota é {num1} .");
 
-            Console.WriteLine(num2+num5);
+            Console.WriteLine("Digite sua segunda nota: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua terceira nota: ");
+            num4 = int.Parse(Console.ReadLine());
+
+            media = (num1 + num3 + num4)/3;
+
+            Console.WriteLine("Sua média é de " + media + ".");
+
         }
     }
 }
