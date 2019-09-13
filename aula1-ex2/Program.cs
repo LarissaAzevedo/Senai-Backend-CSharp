a27a96bcfbbdd2ace85898f0af3304a919ff8adf
FIC <FIC@info.local>
2019-09-13 11:39:55 -0300

    exercicio 2

---
+++ aula1-ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula1_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float valor, acrescimo, venda;
+
+            Console.WriteLine("Insira o valor do produto: ");
+            valor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o percentual de acréscimo: ");
+            acrescimo = float.Parse(Console.ReadLine());
+
+            venda = valor + (valor / acrescimo);
+
+            Console.WriteLine($"O valor de venda é de R${venda}.");
+
+        }
+    }
+}
