44467f1fbf35a68e2d3f3bc214df5e861fb48408
FIC <FIC@info.local>
2019-09-13 13:45:17 -0300

    exercicio 6

---
+++ aula1-ex6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula1_ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int salario, divida, resto;
+
+            Console.WriteLine("Informe o seu salário: ");
+            salario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora informe o valor da sua dívida: ");
+            divida = int.Parse(Console.ReadLine());
+
+            resto = salario - divida;
+
+            Console.WriteLine($"Após pagar sua dívida, restará R${resto}.");
+        }
+    }
+}
