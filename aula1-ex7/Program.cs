9f126dba6a8e3772261663799a55169190c8f24a
FIC <FIC@info.local>
2019-09-13 13:51:03 -0300

    exercicio 7

---
+++ aula1-ex7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula1_ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NPassageiros;
+            double passagem = 4.30, ValorArrecadado;
+
+            Console.WriteLine("Informe quantos passageiros entraram: ");
+            NPassageiros = int.Parse(Console.ReadLine());
+
+            ValorArrecadado = NPassageiros * passagem;
+
+            Console.WriteLine($"O valor arrecadado foi de R${ValorArrecadado}.");
+        }
+    }
+}
