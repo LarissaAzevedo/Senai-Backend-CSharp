b11da30571f672c6c652b9363b1644c5727e31b6
FIC <FIC@info.local>
2019-09-13 11:30:07 -0300

    exercicio 1

---
+++ aula1-ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula1_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Digite um número:");
+            n1 = int.Parse(Console.ReadLine());
+
+            n2 = n1 * 3;
+
+            Console.WriteLine($"O triplo do número digitado é {n2}");
+
+            
+        }
+    }
+}
