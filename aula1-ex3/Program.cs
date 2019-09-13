6294bd9074102491439785fbbcd628303147f4a4
FIC <FIC@info.local>
2019-09-13 11:48:26 -0300

    exercicio 3

---
+++ aula1-ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula1_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, vendas, total;
+
+            Console.WriteLine("Digite o valor do seu salário: ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite quantas vendas foram feitas: ");
+            vendas = double.Parse(Console.ReadLine());
+
+            total = salario + (vendas * 0.5);
+
+            Console.WriteLine($"Você receberá R${total}.");
+        }
+    }
+}
