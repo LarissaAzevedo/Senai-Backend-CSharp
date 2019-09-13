908ac13a40bbbbeb46c1fc86474632ea0e42075b
FIC <FIC@info.local>
2019-09-13 11:53:52 -0300

    exercicio 4

---
+++ aula1-ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula1_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double real, dolar, valor;
+
+            Console.WriteLine("Digite o valor a ser convertido: ");
+            real = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a cotação atual do dólar: ");
+            dolar = double.Parse(Console.ReadLine());
+
+            valor = real * dolar;
+
+            Console.WriteLine($"O valor convetido é de U${valor}.");
+        }
+    }
+}
