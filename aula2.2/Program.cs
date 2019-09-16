4595d648c737ea43ecbf5267b2a993d717763275
FIC <FIC@info.local>
2019-09-16 14:29:45 -0300

    estrutura de decisão - ex 2

---
+++ aula2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pago;
+
+            Console.WriteLine("Insira o valor a ser pago: ");
+            pago = int.Parse(Console.ReadLine());
+
+            if (pago > 100){
+                pago = pago - 25;
+            }
+            Console.WriteLine($"O valor a ser pago é de R${pago}.");
+        }
+    }
+}
