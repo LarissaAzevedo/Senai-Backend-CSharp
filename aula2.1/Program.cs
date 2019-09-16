fde46c11e79d7b56763561b983dd62ed9da59cfe
FIC <FIC@info.local>
2019-09-16 14:25:36 -0300

    estrutura de decisão - ex 1

---
+++ aula2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace aula2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario, despesa, saldo;
+            bool result = false;
+            string resultado = "Saldo negativo - Déficit";
+
+            Console.WriteLine("Digite seu salário: ");
+            salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua despesa: ");
+            despesa = float.Parse(Console.ReadLine());
+
+            saldo = salario - despesa;
+
+            if(saldo > 0){
+                result = true;
+            }
+
+            if(result == true){
+                resultado = "Saldo positivo - Superávit";
+            }
+            Console.Write(resultado);
+
+        }
+    }
+}
