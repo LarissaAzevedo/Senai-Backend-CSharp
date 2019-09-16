406cee1f77c5d1238ab9ef54c384b27f359e7317
FIC <FIC@info.local>
2019-09-16 14:47:37 -0300

    estrutura de decisão - ex 5

---
+++ aula2.5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace aula2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+
+            Console.WriteLine("Digite um valor: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro valor: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite mais um valor: ");
+            n3 = int.Parse(Console.ReadLine());
+
+            //n1 maior valor
+            if(n1 > n2 && n1 > n3){
+                Console.WriteLine($"O maior valor digitado foi {n1}.");
+            }
+
+            //n2 maior valor
+            if(n2 > n1 && n2 > n3){
+                Console.WriteLine($"O maior valor digitado foi {n2}.");
+            }
+
+            //n3 maior valor
+            if(n3 > n1 && n3 > n2){
+                Console.WriteLine($"O maior valor digitado foi {n3}.");
+            }
+        }
+    }
+}
