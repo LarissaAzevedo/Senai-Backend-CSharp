0c8530ea98f8f9abd2cc5c9af2bdb447534326df
FIC <FIC@info.local>
2019-09-16 14:14:08 -0300

    exercicio entrada maior de idade

---
+++ aula2-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace aula2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool entrada = false;
+            int idade;
+            string retorno = "Entrada não permitida";
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if(idade > 17){
+                entrada = true;
+            }
+
+            if(entrada == true){
+                retorno = "Entrada permitida!";
+            } 
+            Console.WriteLine(retorno);
+
+        }
+    }
+}
