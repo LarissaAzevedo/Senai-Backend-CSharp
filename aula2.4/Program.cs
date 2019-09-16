ad281f0a02de7aee91e36d68e2429d772201c234
FIC <FIC@info.local>
2019-09-16 14:40:15 -0300

    estrutura de decisão

---
+++ aula2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            Console.WriteLine("Informe o valor a ser pago: ");
+            valor = double.Parse(Console.ReadLine());
+
+            if(valor > 100 && valor < 1000){
+                valor = valor - (valor * 0.15);
+            }
+
+            Console.WriteLine($"O valor a ser pago é de R${valor}.");
+        }
+    }
+}
