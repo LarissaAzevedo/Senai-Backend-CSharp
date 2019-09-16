da1e0d8a7cde97e9d9355aabec7c6f204a474247
FIC <FIC@info.local>
2019-09-16 15:12:50 -0300

    estrutura de decisão - ex 6

---
+++ aula2.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            
+            Console.WriteLine("Informe a sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if(idade < 10){
+                Console.WriteLine("Sua classificação é: Everyone");
+            }
+            if(idade >= 10 && idade <= 13){
+                Console.WriteLine("Sua classificação é: E 10+");
+            }
+            if(idade > 13 && idade <= 17){
+                Console.WriteLine("Sua classificação é: Teen");
+            }
+            if(idade > 17){
+                Console.WriteLine("Sua classificação é: Mature 17+");
+            }            
+        }
+    }
+}
