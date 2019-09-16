f954aec284239d8801ed6c7d33349c21f41681d4
FIC <FIC@info.local>
2019-09-16 14:34:39 -0300

    estrutura de decisão - ex 3

---
+++ aula2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Digite um número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            if(n1 > n2){
+                Console.WriteLine($"O maior valor digitado foi {n1}");
+            } else{
+                Console.WriteLine($"O maior valor digitado foi {n2}");
+            }
+        }
+    }
+}
