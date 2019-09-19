759956ab8f01801020afab95188eaaf9fce37dc1
FIC <FIC@info.local>
2019-09-19 09:37:08 -0300

    for - incremento e decremento

---
+++ aula4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //laço contado - incremento 
+            for(int cont = 1; cont <= 100; cont++){
+                if(cont % 2 != 0){
+                    Console.WriteLine(cont);
+                }
+            }
+            //laço contado - decremento 
+
+            for(int cont = 100; cont >= 0; cont--){
+                if(cont % 2 != 0){
+                    Console.WriteLine(cont);
+                }
+            }
+        }
+    }
+}
