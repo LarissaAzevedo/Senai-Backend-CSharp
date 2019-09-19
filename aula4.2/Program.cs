074fbf75d5bd864eccfffe838c10a647dbf5a027
FIC <FIC@info.local>
2019-09-19 13:58:46 -0300

    estrutura de repetição - ex 2

---
+++ aula4.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            Console.WriteLine("Digite um número: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nEsta é a tabuada de {n}");
+
+            for(int i = 0; i < 10; i++){
+                cont++;
+                Console.WriteLine(n + " X " + cont + " = " + (cont*n));
+            }
+        }
+    }
+}
