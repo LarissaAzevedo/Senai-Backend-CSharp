4e4369ea1a8f59761a14789984cfefde58c25ed8
FIC <FIC@info.local>
2019-09-19 16:14:49 -0300

    estrutura de repetição - ex 7

--- aula4.7/Program.cs
+++ aula4.7/Program.cs
@@ -8,23 +8,19 @@ namespace aula4._7 {
             int soma = 0;
             int produto = 1;
 
-            for (i = 0; i <= 5; i++) {
+            for (i = 0; i <= 100; i++) {
                 Console.WriteLine ("Digite um número maior que 0.");
                 n = int.Parse (Console.ReadLine ());
 
                 if (n % 2 != 0) { //impar
                     soma += n;
                 }
-
                 if (n % 3 == 0) { //multiplo de 3
                     produto = produto * n;
                 }
-
             }
-
             Console.WriteLine ($"Soma dos números ímpares: {soma}");
             Console.WriteLine ($"Produto dos números múltiplos de 3: {produto}");
-
         }
     }
 }
