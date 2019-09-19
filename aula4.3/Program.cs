f86df64b2e9dcd7be682babb3cce65a212aa3329
FIC <FIC@info.local>
2019-09-19 16:20:52 -0300

    estrutura de repetição - ex 3

---
+++ aula4.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula4._3 {
+    class Program {
+        static void Main (string[] args) {
+            int n = 1;
+            int cont = 0;
+
+            Console.WriteLine ("Digite um número: ");
+            cont = int.Parse (Console.ReadLine ());
+
+            do {
+                if (n % 5 == 0) {
+                    Console.WriteLine (n);
+                }
+                n++;
+            } while (n <= cont);
+
+        }
+    }
+}
