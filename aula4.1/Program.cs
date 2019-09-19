ab5cad41b9113ab2384db2dff8004e79deffc736
FIC <FIC@info.local>
2019-09-19 13:47:21 -0300

    estrutura de repetição - ex 1

---
+++ aula4.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serão exibidos os cubos dos números de 0 a 100");
+            for(int i = 0; i <= 100; i++){
+                Console.WriteLine(i + "³ = " + (i*i*i));
+            }
+        }
+    }
+}
