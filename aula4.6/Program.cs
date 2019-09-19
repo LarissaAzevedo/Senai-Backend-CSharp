3245c6791cd6117b696486da7cb23491d559bf20
FIC <FIC@info.local>
2019-09-19 16:52:32 -0300

    estrutura de repetição - ex 6

---
+++ aula4.6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula4._6 {
+    class Program {
+        static void Main (string[] args) {
+            int i = 0;
+            int fn = 1;
+            int ant = 0;
+            int atual = 0;
+
+            Console.WriteLine ("Sequencia de Fibonacci");
+            for (i = 0; i <= 10; i++) {
+                ant = atual;
+                atual = fn ;
+                fn = ant + atual;
+
+                Console.WriteLine (fn);
+            }
+        }
+    }
+}
