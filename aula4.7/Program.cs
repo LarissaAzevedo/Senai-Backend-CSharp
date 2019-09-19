4e388ccd2c9ffbfbf9304fdb09dec75e4a613986
FIC <FIC@info.local>
2019-09-19 16:13:21 -0300

    estrutura de decisão - ex 7

---
+++ aula4.7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace aula4._7 {
+    class Program {
+        static void Main (string[] args) {
+            int n = 0;
+            int i = 0;
+            int soma = 0;
+            int produto = 1;
+
+            for (i = 0; i <= 5; i++) {
+                Console.WriteLine ("Digite um número maior que 0.");
+                n = int.Parse (Console.ReadLine ());
+
+                if (n % 2 != 0) { //impar
+                    soma += n;
+                }
+
+                if (n % 3 == 0) { //multiplo de 3
+                    produto = produto * n;
+                }
+
+            }
+
+            Console.WriteLine ($"Soma dos números ímpares: {soma}");
+            Console.WriteLine ($"Produto dos números múltiplos de 3: {produto}");
+
+        }
+    }
+}
