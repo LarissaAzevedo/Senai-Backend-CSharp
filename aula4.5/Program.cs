a4bfa2fc9fa688e1682837ae293456a5a89d6540
FIC <FIC@info.local>
2019-09-19 15:40:12 -0300

    estrutura de repetição - ex 5

---
+++ aula4.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace aula4._5 {
+    class Program {
+        static void Main (string[] args) {
+            string nome = "";
+            float salario = 0;
+            float media = 0;
+            string sair = "";
+
+            while (sair != "sim") {
+
+                Console.WriteLine ("Digite seu nome: ");
+                nome = Console.ReadLine ();
+
+                Console.WriteLine ("Digite seu salário: ");
+                salario = int.Parse (Console.ReadLine ());
+
+                media += salario;
+                Console.WriteLine ("Deseja sair da aplicação? Sim ou Não");
+                sair = Console.ReadLine ().ToLower ();
+            }
+
+            Console.WriteLine ($"A media dos salários é de {media}");
+        }
+    }
+}
