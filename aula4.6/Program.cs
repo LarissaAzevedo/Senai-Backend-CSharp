36e358b583d9631a4beb822281aa014ad7027f9e
FIC <FIC@info.local>
2019-09-20 09:20:36 -0300

    estrutura de repetição - ex 6

--- aula4.6/Program.cs
+++ aula4.6/Program.cs
@@ -8,14 +8,43 @@ namespace aula4._6 {
             int ant = 0;
             int atual = 0;
 
-            Console.WriteLine ("Sequencia de Fibonacci");
+            int j = 0;
+            int fn2 = 1;
+            int ant2 = 0;
+            int atual2 = 0;
+
+            int k = 0;
+            int fn3 = 1;
+            int ant3 = 0;
+            int atual3 = 0;
+
+            Console.WriteLine ("Sequencia de Fibonacci - Com for");
             for (i = 0; i <= 10; i++) {
                 ant = atual;
-                atual = fn ;
+                atual = fn;
                 fn = ant + atual;
 
                 Console.WriteLine (fn);
             }
+
+            Console.WriteLine ("Sequencia de Fibonacci - Com while");
+            while ( j <= 10) {
+                ant2 = atual2;
+                atual2 = fn2;
+                fn2 = ant2 + atual2;
+                j++;
+                Console.WriteLine (fn2);
+            }
+
+            Console.WriteLine ("Sequencia de Fibonacci - Com do while");
+            do {
+                ant3 = atual3;
+                atual3 = fn3;
+                fn3 = ant3 + atual3;
+                k++;
+                Console.WriteLine (fn3);
+            } while (k <= 10);
+
         }
     }
 }
