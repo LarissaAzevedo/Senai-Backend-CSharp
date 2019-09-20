e18033c5bca267a35f5e881f714bf42f1769c0e4
FIC <FIC@info.local>
2019-09-20 14:29:29 -0300

    vetor - ex 1

---
+++ aula5.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula5._1 {
+    class Program {
+        static void Main (string[] args) {
+            int[] valores = new int[10];
+
+            for (int i = 0; i <= 9; i++) {
+                Console.WriteLine ("Digite um valor: ");
+                valores[i] = int.Parse (Console.ReadLine ());
+            }
+
+            for (int i = 0; i <= 9; i++) {
+                Console.WriteLine ("O valor do índice " + i + " é " + valores[i]);
+            }
+            Console.WriteLine ("O valor do último índice multiplicado por 5 é " + valores[9] * 5);
+
+        }
+    }
+}
