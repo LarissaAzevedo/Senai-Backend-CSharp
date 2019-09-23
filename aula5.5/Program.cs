c8b43530b73865e411837b3ce3e9769175490df6
FIC <FIC@info.local>
2019-09-23 10:15:59 -0300

    vetor - ex 5

---
+++ aula5.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+            int cont = 0;
+
+            for(int i = 0; i <=9; i++){
+                cont++;
+                vetor[i] = (cont - 1) * 2;
+            }
+            for(int i = 0; i <= 9; i++){
+                Console.WriteLine("Índice " + i + " - " + vetor[i]);
+            } //arrumar
+        }
+    }
+}
