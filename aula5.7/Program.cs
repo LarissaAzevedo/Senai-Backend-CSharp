1de645e6a91e7a51556f2e4f15a6de21aa1816d8
FIC <FIC@info.local>
2019-09-23 11:20:04 -0300

    vetor - ex 7

--- aula5.7/Program.cs
+++ aula5.7/Program.cs
@@ -6,18 +6,23 @@ namespace aula5._7 {
             int[] vetor1 = new int[10];
             int indice = 0;
             int maior = 0;
-            int i = 0;
+            // int i = 0;
 
-            for ( i = 0; i <= 9; i++) {
+            for (int i = 0; i <= 9; i++) {
                 Console.WriteLine ("Insira um valor: ");
                 vetor1[i] = int.Parse (Console.ReadLine ());
-                //maior = vetor1[i];
+
+                if(i == 0){
+                    maior = vetor1[i];
+                }
+
                 if (maior < vetor1[i]) {
                     indice = i;
                     maior = vetor1[i];
                 }
             }
             Console.WriteLine(maior + " é o maior número, índice =  " + indice);
+            Console.WriteLine($"{maior} é o maior valor, índice {indice}");
         }
     }
 }
