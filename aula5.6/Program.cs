fccd6e184e3472a114a8aeae78772eea2835cf64
FIC <FIC@info.local>
2019-09-23 10:05:04 -0300

    vetor - ex 6

---
+++ aula5.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace aula5._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = new int[10];
+            int[] vetor2 = new int[10];
+            int[] vetor3 = new int[10];
+
+            for(int i = 0; i <=9; i++){
+                Console.WriteLine("Vetor 1 - Insira um valor: ");
+                vetor1[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i <=9; i++){
+                Console.WriteLine("Vetor 2 - Insira um valor: ");
+                vetor2[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i <=9; i++){
+                vetor3[i] = vetor1[i] + vetor2[i];
+            }
+            for(int i = 0; i <= 9; i++){
+                Console.WriteLine(vetor3[i]);
+            }
+        }
+    }
+}
