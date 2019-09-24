aeaac468b0a67f6fbdf30f759c3e154d81d677c1
FIC <FIC@info.local>
2019-09-24 14:45:10 -0300

    validação cpf

---
+++ aula6.cpf/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace aula6.cpf {
+    class Program {
+        static void Main (string[] args) {
+
+            Console.WriteLine ("Digite o CPF: ");
+            string cpf = Console.ReadLine ();
+
+            Console.WriteLine (ValidaCPF (cpf));
+        }
+
+        static bool ValidaCPF (string cpfusuario) {
+            int[] v1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            bool resultado = false;
+            string cpfCalculo = "";
+            int resto = 0;
+            int calculo = 0;
+            string digitoV1 = "";
+            string digitoV2 = "";
+
+            // cpfusuario = cpfusuario.Trim (".");
+            // cpfusuario = cpfusuario.Trim ("-");
+
+            cpfCalculo = cpfusuario.Substring (0, 9);
+
+            for (int i = 0; i <= 8; i++) {
+                calculo += int.Parse (cpfCalculo[i].ToString ()) * v1[i];
+            }
+            resto = calculo % 11;
+            calculo = 11 - resto;
+
+            if (calculo > 9) {
+                digitoV1 = "0";
+            } else {
+                digitoV1 = calculo.ToString ();
+            }
+
+            if (digitoV1 == cpfusuario[9].ToString ()) {
+                resultado = true;
+            }
+
+            int[] v2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            resto = 0;
+            cpfCalculo = cpfCalculo + calculo.ToString ();
+            calculo = 0;
+
+            for (int i = 0; i <= 9; i++) {
+                calculo += int.Parse (cpfCalculo[i].ToString ()) * v2[i];
+            }
+            resto = calculo % 11;
+            calculo = 11 - resto;
+
+            if (calculo > 9) {
+                digitoV2 = "0";
+            } else {
+                digitoV2 = calculo.ToString ();
+            }
+
+            if (digitoV2 == cpfusuario[9].ToString ()) {
+                resultado = true;
+            }
+
+            return resultado;
+        }
+    }
+}
