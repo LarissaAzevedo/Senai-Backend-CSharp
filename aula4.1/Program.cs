c8c0fa232c57328c06b0d3302db00fb91b9d684e
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-25 10:14:27 -0300

    Repetição - ex 1

--- aula4.1/Program.cs
+++ aula4.1/Program.cs
@@ -7,6 +7,7 @@ namespace aula4._1
         static void Main(string[] args)
         {
             Console.WriteLine("Serão exibidos os cubos dos números de 0 a 100");
+            
             for(int i = 0; i <= 100; i++){
                 Console.WriteLine(i + "³ = " + (i*i*i));
             }
