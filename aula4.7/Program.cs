6ea696c7e77531c16849bfec27dc8dce98a5ed8a
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-25 10:19:15 -0300

    Repetição - ex 7

--- aula4.7/Program.cs
+++ aula4.7/Program.cs
@@ -3,12 +3,14 @@
 namespace aula4._7 {
     class Program {
         static void Main (string[] args) {
+
             int n = 0;
             int i = 0;
             int soma = 0;
             int produto = 1;
 
             for (i = 0; i <= 100; i++) {
+                
                 Console.WriteLine ("Digite um número maior que 0.");
                 n = int.Parse (Console.ReadLine ());
 
