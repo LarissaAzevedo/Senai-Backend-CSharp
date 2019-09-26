3bf04da11c9e9e6c137c61d0332465d3084f79e6
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-26 10:05:06 -0300

    Try catch

---
+++ aula8.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aula8._1 {
+    class Program {
+        static void Main (string[] args) {
+
+            //tratamento de erro
+            int numero = 0;
+
+            try {
+                Console.WriteLine ("Digite um número: ");
+                numero = int.Parse (Console.ReadLine ());
+
+                Console.WriteLine ("Aeee!");
+
+            } catch (Exception ex) {
+                //a variável ex armazena o erro e é exibido na mensagem quando dá erro
+
+                Console.WriteLine ("Ops, tu é corno!" + ex);
+
+            }
+
+        }
+    }
+}
