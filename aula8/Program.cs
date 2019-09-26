17b4aa4cf848305ee69ed84941c5625717b5258c
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-26 09:41:49 -0300

    Foreach

---
+++ aula8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace aula8 {
+    class Program {
+        static void Main (string[] args) {
+
+            string[] v1 = { "A", "B", "C", "D", "E" };
+
+            //foreach apenas lê, não tem contador (para cada)
+            //o vetor não tem um limite, ele vai ler enquanto houver dados para serem exibidos
+            foreach (string xpto in v1) {
+                Console.WriteLine (xpto);
+            }
+        }
+    }
+}
