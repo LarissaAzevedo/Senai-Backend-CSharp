9dbeb12e7795c8d6020de14d74026b5233518828
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-27 14:07:32 -0300

    Função - ex 5

---
+++ aula9.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula9._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetor = {1, 5, 6.5, 3, 3.3, 4.2, 1.9};
+            Console.WriteLine(Total(vetor));
+        }
+
+        static double Total(double[] v1){
+            double soma = 0;
+
+            for(int i = 0; i < v1.Length; i++){
+                soma += v1[i];
+            }
+
+            return soma;
+        }
+    }
+}
