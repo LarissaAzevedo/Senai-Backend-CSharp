ccec84b09e2a147e9f646ecb5ca1aeecb3449b16
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-27 14:18:33 -0300

    Função - ex 1

---
+++ aula9.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace aula9._1
+{
+    class Program
+    {
+        static void Main (string[] args) {
+
+            Console.WriteLine ("Insira um valor:");
+            double valor = double.Parse (Console.ReadLine ());
+
+            System.Console.WriteLine ("Digite uma porcentagem:");
+            double porcentagem = double.Parse (Console.ReadLine ());
+
+            System.Console.WriteLine("Valor a ser pago: R$" + Desconto (valor,porcentagem));
+        }
+
+        /// <summary>
+        /// Calcula o desconto com os valores que o usuário digitou
+        /// </summary>
+        /// <param name="valor">Valor a ser aplicado desconto</param>
+        /// <param name="porcentagem">Percentual de desconto</param>
+        /// <returns>Desconto sobre o valor</returns>
+        static double Desconto (double val, double por) {
+            double desconto;
+
+            desconto = val - (val * (por / 100));
+
+            return desconto;
+
+        }
+    }
+}
