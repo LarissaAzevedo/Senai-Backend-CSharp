7f671d7f933aa575bb7dd6759b9db64160c4a9fd
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-27 11:29:47 -0300

    Função - ex 3

---
+++ aula9.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula9._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MostraInfo();
+            MostraInfo("Larissa", "Azevedo");
+            MostraInfo(20, 51, 1.73);
+        }
+
+        static void MostraInfo(){
+            System.Console.WriteLine("A razão social da escola Senai...");
+        }
+
+        static  void  MostraInfo(string nome, string sobrenome){
+            
+            Console.WriteLine($"Olá {nome} {sobrenome}!");
+        }
+
+        static void  MostraInfo (int idade, double peso, double altura){
+            
+            Console.WriteLine($"Idade: {idade}\nPeso: {peso}\nAltura: {altura}");
+        }
+    }
+}
