1d61d393a0bd57bd691a227ea8bbaf9ccca09346
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-27 11:44:13 -0300

    Funçã- ex 2

---
+++ aula9.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula9._2 {
+    class Program {
+        static void Main (string[] args) {
+            DiaDaSemana();
+        }
+
+        static void DiaDaSemana () {
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+        }
+        
+    }
+}
