11c7ec7be01259df4046c49bc4721fbfcdc6d0bd
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-09-30 09:11:04 -0300

    Manipulação de arquivos - ex 1

--- aula7.2/Program.cs
+++ aula7.2/Program.cs
@@ -47,7 +47,8 @@ namespace aula7._2 {
             String[][] dados = {
                 new String[] { "Sopa fria", "Sopa cozida só que fria", "Larissa", "R$25,00" },
                 new String[] { "Quiche napolitana", "Achei na net", "Larissa", "R$30,00" },
-                new String[] { "Soja com legumes", "Seguro pra vegetarianos", "Larissa", "R$45,00" }
+                new String[] { "Soja com legumes", "Seguro pra vegetarianos", "Larissa", "R$45,00" },
+                new String[] {"Arroz com ovo","Feito na hora","Larissa","R$9,90"}
             };
 
             //adiciona as células na tabela
@@ -56,6 +57,43 @@ namespace aula7._2 {
             //linha na posição[0] para o cabeçalho
             TableRow Linha1 = tabela.Rows[0];
             Linha1.IsHeader = true;
+
+            //formatação das colunas da tabela, percorre uma a uma
+            for(int i = 0; i < cabecalho.Length; i++){
+                Paragraph p = Linha1.Cells[i].AddParagraph();
+
+                //deixou centralizado tanto na horizontal como na vertical
+                Linha1.Cells[i].CellFormat.VerticalAlignment = VerticalAlignment.Middle;
+                p.Format.HorizontalAlignment = HorizontalAlignment.Center;
+
+                //formatação da fonte dos textos da tabela, fonte segoe e tamanho 14
+                TextRange TR = p.AppendText(cabecalho[i]);
+                TR.CharacterFormat.FontName = "Segoe";
+                TR.CharacterFormat.FontSize = 14;
+            }
+
+            //agora formatação das linhas do corpo da tebela
+            for(int r = 0; r < dados.Length; r++){
+
+                //percorre todas as linhas da tabela, exceto a primeira do cabeçalho
+                TableRow LinhaDados = tabela.Rows[r + 1];
+
+                //percorre as colunas
+                for (int c = 0; c < dados[r].Length; c++)
+                {
+                    //verticalmente centralizado
+                    LinhaDados.Cells[c].CellFormat.VerticalAlignment = VerticalAlignment.Middle;
+
+                    //preenche com os dados nas linhas
+                    Paragraph p2 = LinhaDados.Cells[c].AddParagraph();
+
+                    TextRange TR2 = p2.AppendText(dados[r][c]);
+
+                    //formatação dos dados
+                    p2.Format.HorizontalAlignment = HorizontalAlignment.Center;
+                    
+                }
+            }
 
             #endregion
 
