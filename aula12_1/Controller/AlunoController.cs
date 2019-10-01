38527286ffd2b7bb37e1cea1a977e733d99ec6c2
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2019-10-01 10:12:35 -0300

    POO - ex 1

--- aula12_1/Controller/AlunoController.cs
+++ aula12_1/Controller/AlunoController.cs
@@ -1,45 +1,47 @@
 using System;
 using aula12_1.Model;
 
-namespace aula12_1.Controller
-{
-    public class AlunoController
-    {
-        AlunoModel aluno = new AlunoModel();
+namespace aula12_1.Controller {
+    public class AlunoController {
+        AlunoModel aluno = new AlunoModel ();
 
-        public void LerAluno (){
+        public void LerAluno () {
 
-            System.Console.WriteLine("Digite Seu nome:");
-            aluno.NomeAluno = Console.ReadLine();
-            
-            System.Console.WriteLine("Digite o nome do seu curso:");
-            aluno.NomeCurso = Console.ReadLine();
-            
-            System.Console.WriteLine("Digite a sua idade:");
-            aluno.IdadeAluno = int.Parse(Console.ReadLine());
+            try {
 
-            System.Console.WriteLine("Digite seu RG");
-            aluno.RGAluno = int.Parse(Console.ReadLine());
+                System.Console.WriteLine ("Digite Seu nome:");
+                aluno.NomeAluno = Console.ReadLine ();
 
-            System.Console.WriteLine("Você é bolsista? true ou false");
-            aluno.Bolsista = bool.Parse(Console.ReadLine());                     
-            
+                System.Console.WriteLine ("Digite o nome do seu curso:");
+                aluno.NomeCurso = Console.ReadLine ();
+
+                System.Console.WriteLine ("Digite a sua idade:");
+                aluno.IdadeAluno = int.Parse (Console.ReadLine ());
+
+                System.Console.WriteLine ("Digite seu RG");
+                aluno.RGAluno = int.Parse (Console.ReadLine ());
+
+                System.Console.WriteLine ("Você é bolsista? true ou false");
+                aluno.Bolsista = bool.Parse (Console.ReadLine ());
+
+            } catch (System.Exception ex) {
+                System.Console.WriteLine("Algo deu errado.\n" + ex);
+            }
+
         }
 
-        public void ExibirAluno (){
-            System.Console.WriteLine("Nome: " + aluno.NomeAluno);
-            System.Console.WriteLine("Curso: " + aluno.NomeCurso);
-            System.Console.WriteLine("Idade: " + aluno.IdadeAluno);
-            System.Console.WriteLine("RG: " + aluno.RGAluno);
+        public void ExibirAluno () {
+            System.Console.WriteLine ("Nome: " + aluno.NomeAluno);
+            System.Console.WriteLine ("Curso: " + aluno.NomeCurso);
+            System.Console.WriteLine ("Idade: " + aluno.IdadeAluno);
+            System.Console.WriteLine ("RG: " + aluno.RGAluno);
 
-            if (aluno.Bolsista == true)
-            {
-                System.Console.WriteLine("Você é bolsista.");
-            }else{
-                System.Console.WriteLine("Você não é bolsista.");
+            if (aluno.Bolsista == true) {
+                System.Console.WriteLine ("Você é bolsista.");
+            } else {
+                System.Console.WriteLine ("Você não é bolsista.");
             }
         }
 
-        
     }
 }
